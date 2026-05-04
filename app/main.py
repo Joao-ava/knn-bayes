b20c89eecd7036b6d9aad9024dea30d1268d8db7
pasteldemisto <jdsouz@edu.unifor.br>
2026-05-03 22:24:49 -0300

    add: tables

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from train import train_classification_model, train_regressor_model
+from table import print_table, CLASSIFICATION_LABELS, CLASSIFICATION_COLS, REGRESSION_LABELS, REGRESSION_COLS
 
 if __name__ == '__main__':
     k = 5
     train_classification_model(k)
     train_regressor_model(k)
+    
+    print("\n=== CLASSIFICAÇÃO ===\n")
+    print_table(
+        'classification_table.json',
+        CLASSIFICATION_LABELS,
+        CLASSIFICATION_COLS,
+    )
+
+    print("\n=== REGRESSÃO ===\n")
+    print_table(
+        'regression_table.json',
+        REGRESSION_LABELS,
+        REGRESSION_COLS,
+    )
